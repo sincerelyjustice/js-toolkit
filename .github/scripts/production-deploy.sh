50dfebef06f5bba9a6df7b6fa4f428ddf0882391
sincerelyjustice <sincer3ly.justice@gmail.com>
2023-11-05 19:36:52 +0000

    fix: git config in deployment script

--- .github/scripts/production-deploy.sh
+++ .github/scripts/production-deploy.sh
@@ -13,6 +13,9 @@ git rm -rf .
 cp -r "$TEMP_TOOLKIT"/* .
 rm -rf "$TEMP_TOOLKIT"
 
+git config user.email "sincer3ly.justice@gmail.com"
+git config user.name "sincerelyjustice"
+
 git add -A
 git commit -m "build"
 git push origin HEAD:production --force
